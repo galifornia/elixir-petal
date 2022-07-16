4946e482f5c8cbc55da99c7fd4dc7026f12967e6
galifornia <martinho87@gmail.com>
2022-07-16 14:13:13 +0200

    Setup tailwindcss

---
+++ assets/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['../lib/*_web/**/*.*ex', './js/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
